78468f64fc3a403a466924ed706e92e31e4ccee7
Erwin Dondorp <saltgui@dondorp.com>
2019-02-04 21:14:23 +0100

    cleanup bind statements

--- saltgui/static/scripts/routes/page.js
+++ saltgui/static/scripts/routes/page.js
@@ -4,6 +4,7 @@ class PageRoute extends Route {
     super(path, name, page_selector, menuitem_selector, router);
 
     this._runCommand = this._runCommand.bind(this);
+    this._runningJobs = this._runningJobs.bind(this);
     this._updateJobs = this._updateJobs.bind(this);
     this._updateMinions = this._updateMinions.bind(this);
 
