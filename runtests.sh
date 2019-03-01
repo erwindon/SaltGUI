ed9b85420cafa4da038cb62def728c1b6c4079ae
Sebastiano Marletta <sebastiano.marletta@gmail.com>
2019-03-01 23:19:17 +0100

    Added coverage and coveralls.io stuff

--- runtests.sh
+++ runtests.sh
@@ -17,6 +17,11 @@ $PACKAGE_MANAGER i
 # run lint checks and bundle application
 $PACKAGE_MANAGER run build
 
+# run tests with coverage and report it to coveralls
+$PACKAGE_MANAGER run test:unit
+$PACKAGE_MANAGER run test:coverage 
+$PACKAGE_MANAGER run report:coveralls
+
 # start a salt master, three salt minions and saltgui to run tests on
 docker-compose -f docker/docker-compose.yml up -d
 
@@ -24,6 +29,6 @@ docker-compose -f docker/docker-compose.yml up -d
 $PACKAGE_MANAGER run wait-for-docker
 
 # run the unittests/nightmare.js functional tests
-$PACKAGE_MANAGER run test
+$PACKAGE_MANAGER run test:functional
 
 set +e
