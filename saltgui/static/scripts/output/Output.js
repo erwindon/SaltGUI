15670f2713c62272dd3204b1bc63bdfd4b1d49cd
Erwin Dondorp <saltgui@dondorp.com>
2019-05-27 01:19:22 +0200

    prevent crash on NULL output

--- saltgui/static/scripts/output/Output.js
+++ saltgui/static/scripts/output/Output.js
@@ -234,6 +234,7 @@ export class Output {
       for(const minion in response) {
         const result = response[minion];
         // when full_return is not used, the result is simpler
+        if(result === null) continue;
         if(typeof result !== "object") continue;
         if(!("success" in result)) continue;
         // use keys that can conveniently be sorted
