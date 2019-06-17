78e8e283ddd61a41dd561e78ae4956e497d4f35b
Erwin Dondorp <saltgui@dondorp.com>
2019-06-17 22:36:40 +0200

    allow highlight in SPANs

--- saltgui/static/hilitor/hilitor.js
+++ saltgui/static/hilitor/hilitor.js
@@ -10,7 +10,7 @@ function Hilitor(start, id, tag)
   let targetNode = start;
   if(id) targetNode = targetNode.querySelector(id);
   var hiliteTag = tag || "MARK";
-  var skipTags = new RegExp("^(?:" + hiliteTag + "|SCRIPT|FORM|SPAN)$");
+  var skipTags = new RegExp("^(?:" + hiliteTag + "|SCRIPT|FORM)$");
   var colors = ["#ff6", "#a0ffff", "#9f9", "#f99", "#f6f"];
   var wordColor = [];
   var colorIdx = 0;
