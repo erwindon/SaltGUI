7485b66912a30711daf112fa8b0228bb45e85a53
Erwin Dondorp <saltgui@dondorp.com>
2020-05-22 02:36:41 +0200

    also push the 'latest' labels for the containers on docker-hub

--- docker/build-and-upload.sh
+++ docker/build-and-upload.sh
@@ -3,13 +3,17 @@ set -x
 cd dockerfiles
 set -e
 tag=3000.3
-docker build -f dockerfile-saltmaster --tag erwindon/saltgui-saltmaster:$tag .
-docker build -f dockerfile-saltminion-ubuntu --tag erwindon/saltgui-saltminion-ubuntu:$tag .
-docker build -f dockerfile-saltminion-debian --tag erwindon/saltgui-saltminion-debian:$tag .
-docker build -f dockerfile-saltminion-centos --tag erwindon/saltgui-saltminion-centos:$tag .
-docker image prune --force
-docker push erwindon/saltgui-saltmaster:$tag
-docker push erwindon/saltgui-saltminion-ubuntu:$tag
-docker push erwindon/saltgui-saltminion-debian:$tag
-docker push erwindon/saltgui-saltminion-centos:$tag
+docker build -f dockerfile-saltmaster --tag erwindon/saltgui-saltmaster:$tag --tag erwindon/saltgui-saltmaster:latest .
+docker build -f dockerfile-saltminion-ubuntu --tag erwindon/saltgui-saltminion-ubuntu:$tag --tag erwindon/saltgui-saltminion-ubuntu:latest .
+docker build -f dockerfile-saltminion-debian --tag erwindon/saltgui-saltminion-debian:$tag --tag erwindon/saltgui-saltminion-debian:latest .
+docker build -f dockerfile-saltminion-centos --tag erwindon/saltgui-saltminion-centos:$tag --tag erwindon/saltgui-saltminion-centos:latest .
+docker images | awk "/^<none>/ {print $3;}" | xargs --no-run-if-empty docker rmi
+for t in $tag latest; do
+	docker push erwindon/saltgui-saltmaster:$t
+	docker push erwindon/saltgui-saltminion-ubuntu:$t
+	docker push erwindon/saltgui-saltminion-debian:$t
+	docker push erwindon/saltgui-saltminion-centos:$t
+done
+docker system prune -f
+docker images
 # End
