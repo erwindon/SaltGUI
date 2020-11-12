erwindon/SaltGUI

commit 857deb53bcb82b0425f274ce27affd2bb02c9dbc

recognize categories with the next '.' already present

saltgui/static/scripts/output/OutputDocumentation.js
@@ -7,6 +7,12 @@ export class OutputDocumentation {
   // test whether the returned data matches the requested data
   static _isDocuKeyMatch (pKey, pFilterKey) {
 
+    // user may already have typed the "."
+    // but does not know what to type next
+    if (pFilterKey.endsWith(".")) {
+      pFilterKey = pFilterKey.replace(/[.]$/, "");
+    }
+
     // no filter is always OK
     if (!pFilterKey) {
       return true;

commit f326d567e140ea5f1a41f461bccb8025b9c0a2b1

fixed unit thest that broke in 857deb53bcb82b0425f274ce27affd2bb02c9dbc

saltgui/static/scripts/output/OutputDocumentation.js
@@ -9,7 +9,7 @@ export class OutputDocumentation {
 
     // user may already have typed the "."
     // but does not know what to type next
-    if (pFilterKey.endsWith(".")) {
+    if (pFilterKey && pFilterKey.endsWith(".")) {
       pFilterKey = pFilterKey.replace(/[.]$/, "");
     }
 