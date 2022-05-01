838b4abba0adc4247184f194724a140599fcb25a
Erwin Dondorp <saltgui@dondorp.com>
2022-05-01 02:24:24 +0200

    apply eslint setting no-undef-init; fixed all violations

--- saltgui/static/scripts/Utils.js
+++ saltgui/static/scripts/Utils.js
@@ -357,7 +357,7 @@ export class Utils {
       pSearchText = pSearchText.toUpperCase();
     }
 
-    let regexp = undefined;
+    let regexp;
 
     const errorBox = pTable.parentElement.querySelector(".search-error");
     if (regExpFlag) {
