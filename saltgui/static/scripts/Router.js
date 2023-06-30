3617375d3bca2928c2a3f532a4752c21bc81e406
Erwin Dondorp <saltgui@dondorp.com>
2023-06-30 23:02:54 +0200

    first implementation of flexible reports

--- saltgui/static/scripts/Router.js
+++ saltgui/static/scripts/Router.js
@@ -22,6 +22,8 @@ import {Output} from "./output/Output.js";
 import {PillarsMinionPage} from "./pages/PillarsMinion.js";
 import {PillarsPage} from "./pages/Pillars.js";
 import {ReactorsPage} from "./pages/Reactors.js";
+import {ReportPage} from "./pages/Report.js";
+import {ReportsPage} from "./pages/Reports.js";
 import {SchedulesMinionPage} from "./pages/SchedulesMinion.js";
 import {SchedulesPage} from "./pages/Schedules.js";
 import {TemplatesPage} from "./pages/Templates.js";
@@ -59,6 +61,8 @@ export class Router {
     this._registerPage(Router.reactorsPage = new ReactorsPage(this));
     this._registerPage(Router.optionsPage = new OptionsPage(this));
     this._registerPage(Router.issuesPage = new IssuesPage(this));
+    this._registerPage(Router.reportsPage = new ReportsPage(this));
+    this._registerPage(Router.reportPage = new ReportPage(this));
     this._registerPage(Router.logoutPage = new LogoutPage(this));
 
     this._registerRouterEventListeners();
@@ -212,6 +216,7 @@ export class Router {
     this._registerMenuItem(null, "events", "events", "e");
     this._registerMenuItem("events", "reactors", "reactors", "r");
     this._registerMenuItem(null, "issues", "issues", "i");
+    this._registerMenuItem("issues", "reports", "reports");
     // no shortcut for logout
     this._registerMenuItem(null, "logout", "logout");
   }
@@ -304,6 +309,7 @@ export class Router {
     Router._showMenuItem(pages, Router.highStatePage);
     Router._showMenuItem(pages, Router.templatesPage);
     Router._showMenuItem(pages, Router.issuesPage);
+    Router._showMenuItem(pages, Router.reportsPage);
     Router._showMenuItem(pages, Router.eventsPage, ["reactors"]);
     Router._showMenuItem(pages, Router.reactorsPage);
     Router._showMenuItem(pages, Router.logoutPage);
