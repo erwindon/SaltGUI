34fa508ef9d86595a8100bd15c15653160ec6355
Erwin Dondorp <saltgui@dondorp.com>
2026-06-19 12:11:42 +0200

    bumped to ES2024

--- eslint.config.js
+++ eslint.config.js
@@ -76,7 +76,7 @@ export default [
   {
     files: ["saltgui/static/scripts/**/*.js"],
     languageOptions: {
-      ecmaVersion: 2023,
+      ecmaVersion: 2024,
       sourceType: "module",
     }
   },
