951f97c9de3503b605e7799028ba69d8d200075d
Erwin Dondorp <saltgui@dondorp.com>
2026-08-13 18:46:39 +0200

    fix S1126: ignore - Replace this if-then-else flow by a single return statement

--- saltgui/static/scripts/output/Output.js
+++ saltgui/static/scripts/output/Output.js
@@ -555,7 +555,7 @@ export class Output {
       // note that really old minions do not return 'retcode'
       return pMinionResponse.retcode === 0;
     }
-    if (pMinionResponse?.Error) {
+    if (pMinionResponse?.Error) { // NOSONAR S1126
       // e.g. runners.jobs.list_job blahblah
       return false;
     }
