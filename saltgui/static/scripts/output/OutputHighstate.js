1bcce83ca09470e03947e4bf1d25c17d35710261
Erwin Dondorp <saltgui@dondorp.com>
2026-08-19 02:37:36 +0200

    refactored a OutputHighstate function due to complexity

--- saltgui/static/scripts/output/OutputHighstate.js
+++ saltgui/static/scripts/output/OutputHighstate.js
@@ -102,64 +102,71 @@ export class OutputHighstate {
     }
   }
 
+  static _processVisibleTask (pTask, pMinionId, pJobId, pNr, pDiv, pStats) {
+    const components = pTask.___key___.split("_|-");
+    const functionName = components[0] + "." + components[3];
+    const nrChanges = Output.getTaskNrChanges(pTask);
+    pStats.changesDetail += nrChanges;
+
+    const taskId = components[1];
+    let taskName = components[2];
+    if (Output.isStateOutputSelected("_id")) {
+      taskName = taskId;
+    }
+    // might be a grouped entry, then show the count
+    if (pTask.cnt) {
+      taskName += " (" + pTask.cnt + ")";
+    }
+
+    const taskSpan = OutputHighstate._selectTaskOutput(pTask, taskId, taskName, functionName, pMinionId, pJobId, nrChanges);
+    taskSpan.classList.add(Output.getTaskClass(pTask));
+
+    if (pTask.result && nrChanges) {
+      pStats.changesSummary += 1;
+    }
+
+    const taskDiv = Utils.createDiv("", "", Utils.getIdFromMinionId(pMinionId + "." + pNr));
+    taskDiv.append(taskSpan);
+    pDiv.append(taskDiv);
+  }
+
   static _accumulateTaskStats (pTasks, pMinionId, pJobId, pDiv) {
-    let succeeded = 0;
-    let failed = 0;
-    let skipped = 0;
-    let totalMilliSeconds = 0;
-    let changesSummary = 0;
-    let changesDetail = 0;
-    let hidden = 0;
+    const stats = {
+      changesDetail: 0,
+      changesSummary: 0,
+      failed: 0,
+      hidden: 0,
+      skipped: 0,
+      succeeded: 0,
+      totalMilliSeconds: 0,
+    };
+
     let nr = 0;
 
     for (const task of pTasks) {
       nr += 1;
 
       if (task.duration) {
-        totalMilliSeconds += task.duration;
+        stats.totalMilliSeconds += task.duration;
       }
 
       if (task.result === null) {
-        skipped += 1;
+        stats.skipped += 1;
       } else if (task.result) {
-        succeeded += 1;
+        stats.succeeded += 1;
       } else {
-        failed += 1;
+        stats.failed += 1;
       }
 
       if (Output.isHiddenTask(task)) {
-        hidden += 1;
+        stats.hidden += 1;
         continue;
       }
 
-      const components = task.___key___.split("_|-");
-      const functionName = components[0] + "." + components[3];
-      const nrChanges = Output.getTaskNrChanges(task);
-      changesDetail += nrChanges;
-
-      const taskId = components[1];
-      let taskName = components[2];
-      if (Output.isStateOutputSelected("_id")) {
-        taskName = taskId;
-      }
-      // might be a grouped entry, then show the count
-      if (task.cnt) {
-        taskName += " (" + task.cnt + ")";
-      }
-
-      const taskSpan = OutputHighstate._selectTaskOutput(task, taskId, taskName, functionName, pMinionId, pJobId, nrChanges);
-      taskSpan.classList.add(Output.getTaskClass(task));
-
-      if (task.result && nrChanges) {
-        changesSummary += 1;
-      }
-
-      const taskDiv = Utils.createDiv("", "", Utils.getIdFromMinionId(pMinionId + "." + nr));
-      taskDiv.append(taskSpan);
-      pDiv.append(taskDiv);
+      OutputHighstate._processVisibleTask(task, pMinionId, pJobId, nr, pDiv, stats);
     }
 
-    return { changesDetail, changesSummary, failed, hidden, skipped, succeeded, totalMilliSeconds };
+    return { changesDetail: stats.changesDetail, changesSummary: stats.changesSummary, failed: stats.failed, hidden: stats.hidden, skipped: stats.skipped, succeeded: stats.succeeded, totalMilliSeconds: stats.totalMilliSeconds };
   }
 
   static _addSummary (pDiv, pStats) {
