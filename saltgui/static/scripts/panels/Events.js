bc20402415d9907f87083267698bbfb03bb30baa
Erwin Dondorp <saltgui@dondorp.com>
2026-08-20 01:17:33 +0200

    show how many events were missed during pause, closes #950

--- saltgui/static/scripts/panels/Events.js
+++ saltgui/static/scripts/panels/Events.js
@@ -23,10 +23,20 @@ export class EventsPanel extends Panel {
     this.addMsg();
 
     this.setPlayPauseButton("pause");
+
+    this.nrEvents = 0;
+    this.missedEvents = 0;
+    this.missedEventsRow = null;
+    this.missedEventsRowDataTd = null;
+    this.missedEventsRowTimestampSpan = null;
   }
 
   onShow () {
     this.nrEvents = 0;
+    this.missedEvents = 0;
+    this.missedEventsRow = null;
+    this.missedEventsRowDataTd = null;
+    this.missedEventsRowTimestampSpan = null;
     this.updateFooter();
   }
 
@@ -52,12 +62,56 @@ export class EventsPanel extends Panel {
   }
 
   handleAnyEvent (pTag, pData) {
+    const tbody = this.table.tBodies[0];
 
     if (this.playOrPause !== "play") {
+      this.missedEvents += 1;
+      const missedTxt = Utils.txtZeroOneMany(this.missedEvents, "", "{0} missed event", "{0} missed events");
+      if (this.missedEventsRow === null) {
+        const tr = Utils.createTr();
+        tr.classList.add("missed-events");
+
+        const stampTd = Utils.createTd();
+        const stampSpan = Utils.createSpan();
+        const stampDate = new Date();
+        Output.dateTimeStr(stampDate, stampSpan, "bottom-left", true);
+        stampTd.appendChild(stampSpan);
+        tr.append(stampTd);
+
+        const tagTd = Utils.createTd();
+        const sinceTextSpan = Utils.createSpan();
+        sinceTextSpan.innerText = "Since ";
+        tagTd.appendChild(sinceTextSpan);
+        const sinceTimeSpan = Utils.createSpan();
+        Output.dateTimeStr(stampDate, sinceTimeSpan, "bottom-left", true);
+        tagTd.appendChild(sinceTimeSpan);
+        tr.append(tagTd);
+
+        const dataTd = Utils.createTd("event-data", missedTxt);
+        tr.append(dataTd);
+        tbody.prepend(tr);
+        this.missedEventsRow = tr;
+        this.missedEventsRowDataTd = dataTd;
+        this.missedEventsRowTimestampSpan = stampSpan;
+      } else {
+        const stampSpan = this.missedEventsRowTimestampSpan;
+        while (stampSpan.firstChild) {
+          stampSpan.removeChild(stampSpan.firstChild);
+        }
+        const stampDate = new Date();
+        Output.dateTimeStr(stampDate, stampSpan, "bottom-left", true);
+        this.missedEventsRowDataTd.innerText = missedTxt;
+      }
       return;
     }
 
-    const tbody = this.table.tBodies[0];
+    if (this.missedEventsRow !== null) {
+      this.missedEventsRow = null;
+      this.missedEventsRowDataTd = null;
+      this.missedEventsRowTimestampSpan = null;
+      this.missedEvents = 0;
+    }
+
     const tr = Utils.createTr();
 
     // add timestamp value
@@ -83,16 +137,20 @@ export class EventsPanel extends Panel {
     tr.append(dataTd);
 
     tbody.prepend(tr);
+    this.nrEvents += 1;
 
     const searchBlock = this.div.querySelector(".search-box");
     Utils.hideShowTableSearchBar(searchBlock, this.table, "refresh");
 
     // limit to MAX_EVENTS_IN_VIEW rows only
     while (tbody.rows.length > MAX_EVENTS_IN_VIEW) {
+      const deletedRow = tbody.rows[tbody.rows.length - 1];
+      if (!deletedRow.classList.contains("missed-events")) {
+        this.nrEvents -= 1;
+      }
       tbody.deleteRow(tbody.rows.length - 1);
     }
 
-    this.nrEvents = tbody.rows.length;
     this.updateFooter();
   }
 }
